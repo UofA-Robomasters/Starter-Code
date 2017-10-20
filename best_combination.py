ce4fac11086c7451b71917638fd54ba9e1922ff4
AlexQian97 <wenzhangq@gmail.com>
2017-10-19 23:25:32 -0600

    combination added

---
+++ best_combination.py
@@ -0,0 +1,73 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+def cal_heat(v):
+    return v ** 2
+
+def cool_down(heat):
+    return heat - COOL_SPEED
+
+def cal_dmg(v):
+    if 10 <= v <= 25:
+        return v ** 2
+    else:
+        # linear assumed, expected value returned
+        prob_500 = 1 / (45 - 25) * (v - 25)
+        return prob_500 * 500 + (1 - prob_500) * 50
+
+HEAT_LIMIT = 1600
+COOL_SPEED = 500
+
+v_list = np.arange(10, 46)                       # m/s
+f_list = np.linspace(0.1, 5, num=49)             # /s
+# print(v_list)
+# print(f_list)
+
+dmg_array = np.zeros((len(v_list), len(f_list)))
+for v_index, v in enumerate(v_list):
+    for f_index, f in enumerate(f_list):
+    # for f_index, f in enumerate(np.ones(49)*5):
+        dmg = 0
+        heat = 0
+        last_time = 0
+        if f > 1:
+            iter_list = np.linspace(0, 100, num=f*100)
+        else:
+            iter_list = range(0, 100)
+        for t in iter_list:
+            # t_step
+            for i in range(int(1//f)):
+                # try shot
+                if heat + cal_heat(v) < HEAT_LIMIT:
+                    dmg += cal_dmg(v)
+            if t % 1 == 0:
+                heat = cool_down(heat)
+            else:
+                if math.ceil(t) > last_time:
+                    # print("!")
+                    heat = cool_down(heat)
+                    last_time = t
+        dmg_array[v_index][f_index] = dmg / 100
+        # raise EOFError
+# print(dmg_array)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+v_list, f_list = np.meshgrid(f_list, v_list)
+# print(v_list.shape)
+# print(f_list.shape)
+# print(dmg_array.shape)
+surf = ax.plot_surface(v_list, f_list, dmg_array, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+plt.show()
+# Axes3D.plot(v_list, f_list, dmg_array)
+# X = np.arange(-5, 5, 0.25)
+# print(X.shape)
+# Y = np.arange(-5, 5, 0.25)
+# X, Y = np.meshgrid(X, Y)
+# R = np.sqrt(X**2 + Y**2)
+# Z = np.sin(R)
+# print(X.shape)
+# print(Y.shape)
+# print(Z.shape)
