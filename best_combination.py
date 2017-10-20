e165466beac07dbd8fc8955fe1bdc63a10dbae29
AlexQian97 <wenzhangq@gmail.com>
2017-10-20 10:40:06 -0600

    fixed

--- best_combination.py
+++ best_combination.py
@@ -8,7 +8,10 @@ def cal_heat(v):
     return v ** 2
 
 def cool_down(heat):
-    return heat - COOL_SPEED
+    heat = heat - COOL_SPEED / 50
+    if heat < 0:
+        heat = 0
+    return heat
 
 def cal_dmg(v):
     if 10 <= v <= 25:
@@ -23,38 +26,36 @@ COOL_SPEED = 500
 
 v_list = np.arange(10, 46)                       # m/s
 f_list = np.linspace(0.1, 5, num=49)             # /s
-# print(v_list)
-# print(f_list)
 
 dmg_array = np.zeros((len(v_list), len(f_list)))
 for v_index, v in enumerate(v_list):
+# for v_index, v in enumerate(np.ones(36) * 25):
     for f_index, f in enumerate(f_list):
-    # for f_index, f in enumerate(np.ones(49)*5):
+    # for f_index, f in enumerate(np.ones(49)*4):
         dmg = 0
         heat = 0
-        last_time = 0
-        if f > 1:
-            iter_list = np.linspace(0, 100, num=f*100)
-        else:
-            iter_list = range(0, 100)
-        for t in iter_list:
-            # t_step
-            for i in range(int(1//f)):
-                # try shot
+        counter = 0
+        for t in np.linspace(0, 100, num=50*100, endpoint=False):   # 50Hz
+            # print(t, counter, t > counter * 1.0 / f)
+            if t > counter * 1.0 / f:
                 if heat + cal_heat(v) < HEAT_LIMIT:
                     dmg += cal_dmg(v)
-            if t % 1 == 0:
-                heat = cool_down(heat)
-            else:
-                if math.ceil(t) > last_time:
-                    # print("!")
-                    heat = cool_down(heat)
-                    last_time = t
+                    heat += cal_heat(v)
+                    # print(t, heat, v, cal_dmg(v), dmg)
+                counter += 1
+
+            # print('before', heat)
+            heat = cool_down(heat)
+            # print(heat)
+            # if t == 5:
+            #     raise EOFError
+
         dmg_array[v_index][f_index] = dmg / 100
         # raise EOFError
 # print(dmg_array)
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 v_list, f_list = np.meshgrid(f_list, v_list)
-surf = ax.plot_surface(v_list, f_list, dmg_array, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+# surf = ax.plot_surface(v_list, f_list, dmg_array, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+surf = ax.plot_surface(v_list, f_list, dmg_array)
 plt.show()
