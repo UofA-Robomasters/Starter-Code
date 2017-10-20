e684e408d3847ad780ef5fde6e1a51565afffd93
AlexQian97 <wenzhangq@gmail.com>
2017-10-20 13:57:05 -0600

    cleared

--- best_combination.py
+++ best_combination.py
@@ -29,33 +29,22 @@ f_list = np.linspace(0.1, 5, num=49)             # /s
 
 dmg_array = np.zeros((len(v_list), len(f_list)))
 for v_index, v in enumerate(v_list):
-# for v_index, v in enumerate(np.ones(36) * 25):
     for f_index, f in enumerate(f_list):
-    # for f_index, f in enumerate(np.ones(49)*4):
         dmg = 0
         heat = 0
         counter = 0
         for t in np.linspace(0, 100, num=50*100, endpoint=False):   # 50Hz
-            # print(t, counter, t > counter * 1.0 / f)
             if t > counter * 1.0 / f:
                 if heat + cal_heat(v) < HEAT_LIMIT:
                     dmg += cal_dmg(v)
                     heat += cal_heat(v)
-                    # print(t, heat, v, cal_dmg(v), dmg)
                 counter += 1
-
-            # print('before', heat)
             heat = cool_down(heat)
-            # print(heat)
-            # if t == 5:
-            #     raise EOFError
 
         dmg_array[v_index][f_index] = dmg / 100
-        # raise EOFError
-# print(dmg_array)
+
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 v_list, f_list = np.meshgrid(f_list, v_list)
 surf = ax.plot_surface(v_list, f_list, dmg_array, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-# surf = ax.plot_surface(v_list, f_list, dmg_array)
 plt.show()
